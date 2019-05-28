1866590fb13d6547e4617fc18cb7793ad9902a7f
Thiago Krempser <tkrempser@gmail.com>
2019-05-28 16:40:12 -0300

    Solve "Simple Array Sum" problem

---
+++ python-3/simple_array_sum.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import os
+import sys
+from functools import reduce
+
+
+#
+# Complete the simpleArraySum function below.
+#
+def simpleArraySum(ar):
+    #
+    # Write your code here.
+    #
+    sum = 0
+    return reduce(lambda n, m: n + m, ar)
+
+if __name__ == '__main__':
+    fptr = open('os.environ['OUTPUT_PATH']', 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
