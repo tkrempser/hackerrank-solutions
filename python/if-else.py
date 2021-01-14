45aca57ae3caa225844567e88cf89f76f3feb5d6
Thiago Krempser <tkrempser@gmail.com>
2021-01-13 21:28:47 -0300

    Add if-else problem solution

---
+++ python/if-else.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+if __name__ == '__main__':
+  n = int(input().strip())
+
+  if n % 2 == 1:
+    print("Weird")
+  elif n >= 2 and n < 6:
+    print("Not Weird")
+  elif n >= 6 and n < 21:
+    print("Weird")
+  elif n >= 21:
+    print("Not Weird")
